0655c926aab899554484e2cf8c8bfa2b90bacad0
José Alejandro Valduz Contreras <jose.valduz@unet.edu.ve>
2023-08-10 20:47:24 -0400

    Uso de Mutadores y accesores para name en todas tb

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -42,4 +45,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            //Accesor->Transforma la representacion del registro
+            get: fn ($value) => ucwords($value),
+
+            //Mutador
+            set: fn ($value) => strtolower($value)
+        );
+
+        // return new Attribute(
+        //     //Accesor->Transforma la representacion del registro
+        //     get: function ($value) {
+        //         return ucwords($value);
+        //     },
+        //     //Mutador
+        //     set: function ($value) {
+        //         return strtolower($value);
+        //     }
+        // );
+    }
 }
--- app/Models/vivere.php
+++ app/Models/vivere.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class vivere extends Model
 {
     use HasFactory;
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            //Accesor->Transforma la representacion del registro
+            get: fn ($value) => ucwords($value),
+
+            //Mutador
+            set: fn ($value) => strtolower($value)
+        );
+    }
 }
